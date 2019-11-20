64276eef02a54b49b31ae8dd53255ff5135aec81
Amir Amininaji <amir.amininaji@pwc.com>
2019-11-20 14:02:29 +0200

    The basic numbers are working.

--- RomanNumbers/Program.cs
+++ RomanNumbers/Program.cs
@@ -10,22 +10,33 @@ namespace RomanNumbers
             Console.WriteLine("Hello World!");
         }
 
-        public List<int> GetAllNumbers(int number, int divisionValue)
+        public string ConvertToRomanNumber(List<int> numbers)
         {
-            if (number / divisionValue == 0)
+            string result = "";
+            numbers = RemoveZeroInList(numbers);
+            foreach (var number in numbers)
+            {
+                result += NumbersToRomanNumbers()[number];
+            }
+            return result;
+        }
+
+        public List<int> GetAllNumbers(int number, int divisor)
+        {
+            if (number / divisor == 0)
                 return new List<int>();
 
-            List<int> allNumbers = GetAllNumbers(number, divisionValue * 10);
+            List<int> allNumbers = GetAllNumbers(number, divisor * 10);
 
             number -= CalcCurrentValue(allNumbers);
-            allNumbers.Add(CurrentNumber(number, divisionValue));
+            allNumbers.Add(CurrentNumber(number, divisor));
 
             return allNumbers;
         }
 
-        private int CurrentNumber(int number, int divisoin)
+        private int CurrentNumber(int number, int divisor)
         {
-            return (number / divisoin) * divisoin;
+            return (number / divisor) * divisor;
         }
 
         private int CalcCurrentValue(List<int> numbers)
@@ -36,6 +47,51 @@ namespace RomanNumbers
                 result += number;
             }
             return result;
+        }
+
+        private List<int> RemoveZeroInList(List<int> numbers)
+        {
+            while (numbers.Remove(0)) { }
+            return numbers;
+        }
+
+        private Dictionary<string, int> RomanNumbersToNumbers()
+        {
+            return new Dictionary<string, int>()
+            {
+                { "I", 1 },
+                { "V", 5 },
+                { "X", 10 },
+                { "L", 50 },
+                { "C", 100 },
+                { "D", 500 },
+                { "M", 1000 }
+            };
+        }
+
+        private Dictionary<int, string> NumbersToRomanNumbers()
+        {
+            return new Dictionary<int, string>()
+            {
+                { 1, "I" },
+                { 5 , "V" },
+                { 10, "X" },
+                { 50, "L" },
+                { 100, "C" },
+                { 500, "D" },
+                { 1000, "M" }
+            };
+        }
+
+        public enum RomanNumber
+        {
+            I = 1,
+            V = 5,
+            X = 10,
+            L = 50,
+            C = 100,
+            D = 500,
+            M = 1000
         }
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -27,5 +27,30 @@ namespace UnitTestProject1
             v = p.GetAllNumbers(1234567, 1);
             CollectionAssert.AreEqual(new List<int>() { 1000000, 200000, 30000, 4000, 500, 60, 7 }, v);
         }
+
+        [TestMethod]
+        public void ConvertToRomanNumberSimpleTest()
+        {
+            var romanNumber = p.ConvertToRomanNumber(p.GetAllNumbers(1, 1));
+            Assert.AreEqual("I", romanNumber);
+
+            romanNumber = p.ConvertToRomanNumber(p.GetAllNumbers(5, 1));
+            Assert.AreEqual("V", romanNumber);
+
+            romanNumber = p.ConvertToRomanNumber(p.GetAllNumbers(10, 1));
+            Assert.AreEqual("X", romanNumber);
+
+            romanNumber = p.ConvertToRomanNumber(p.GetAllNumbers(50, 1));
+            Assert.AreEqual("L", romanNumber);
+
+            romanNumber = p.ConvertToRomanNumber(p.GetAllNumbers(100, 1));
+            Assert.AreEqual("C", romanNumber);
+
+            romanNumber = p.ConvertToRomanNumber(p.GetAllNumbers(500, 1));
+            Assert.AreEqual("D", romanNumber);
+
+            romanNumber = p.ConvertToRomanNumber(p.GetAllNumbers(1000, 1));
+            Assert.AreEqual("M", romanNumber);
+        }
     }
 }
